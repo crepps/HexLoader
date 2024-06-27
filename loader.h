88c212954b44a4068ac02ece4c027e3b9ec40f42
Kevin Crepps <kevinx.crepps@intel.com>
2024-06-26 22:51:17 -0700

    Started back end, validate paths.

--- loader.h
+++ loader.h
@@ -1,6 +1,28 @@
 #pragma once
+#include <string>
+#include <vector>
+
+#define SUCCESS 0
+#define FAILURE_ABORT 1
+#define FAILURE_CONTINUE 2
 
 class Loader
 {
-	
+private:
+	std::string binPath,
+				runPath;
+	std::vector<std::string> libPaths;
+
+public:
+	enum PATH_TYPE
+	{
+		ALL,
+		PATH_BIN,
+		PATH_LIB,
+		PATH_EXPORT
+	};
+
+	Loader() {};
+	unsigned int ValidatePath(PATH_TYPE);
+	~Loader() {}
 };
