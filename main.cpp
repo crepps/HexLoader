8e894ebe4d4f651e69092993b924bfe93c90f0ff
Kevin Crepps <kevinx.crepps@intel.com>
2024-06-27 23:57:18 -0700

    Added to path validation.

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <Windows.h>
+#include <fstream>
 #include <filesystem>
 #include "loader.h"
 #include "gui.h"
@@ -20,24 +21,44 @@ public:
 
 TEST_F(LoaderTesting, CheckBinPathSet)
 {
-	obj.SetPath(Loader::PATH_BIN, "C:\\temp\\bin.exe");
+	obj.SetPath(Loader::PATH_BIN, "C:\\temp\\hl_mock_bin.exe");
 	ASSERT_THAT(obj.GetBinPath(), Ne(""));
 }
 TEST_F(LoaderTesting, CheckLibPathsSet)
 {
-	obj.SetPath(Loader::PATH_LIB, "C:\\temp\\lib.dll");
+	obj.SetPath(Loader::PATH_LIB, "C:\\temp\\hl_mock_lib1.dll");
 	ASSERT_THAT(obj.GetLibPaths().empty(), Eq(false));
 }
 TEST_F(LoaderTesting, CheckRunPathSet)
 {
-	obj.SetPath(Loader::PATH_RUN, "C:\\temp\\");
+	obj.SetPath(Loader::PATH_RUN, "C:\\temp");
 	ASSERT_THAT(obj.GetRunPath(), Ne(""));
 }
-//TEST_F(LoaderTesting, ValidateBinPath)
-//{
-//	ASSERT_THAT(obj.ValidatePath(Loader::PATH_BIN), Eq(SUCCESS));
-//}
+TEST_F(LoaderTesting, ValidateBinPath)
+{
+	obj.SetPath(Loader::PATH_BIN, "C:\\temp\\hl_mock_bin.exe");
+	std::ofstream outFile(obj.GetBinPath().c_str(), std::ios::out);	// Create exe at set location
+	outFile.close();
+	ASSERT_THAT(obj.ValidatePath(Loader::PATH_BIN), Eq(SUCCESS));
+}
+TEST_F(LoaderTesting, ValidateLibPaths)
+{
+	obj.SetPath(Loader::PATH_LIB, "C:\\temp\\hl_mock_lib1.dll");
+	obj.SetPath(Loader::PATH_LIB, "C:\\temp\\hl_mock_lib2.dll");
 
+	// Create two .dlls at set location
+	std::ofstream outFile1(obj.GetLibPaths()[0].c_str(), std::ios::out),
+					outFile2(obj.GetLibPaths()[1].c_str(), std::ios::out);
+	outFile2.close();
+	outFile1.close();
+
+	ASSERT_THAT(obj.ValidatePath(Loader::PATH_LIB), Eq(SUCCESS));
+}
+TEST_F(LoaderTesting, ValidateRunPath)
+{
+	obj.SetPath(Loader::PATH_RUN, "C:\\temp");
+	ASSERT_THAT(obj.ValidatePath(Loader::PATH_RUN), Eq(SUCCESS));
+}
 #endif
 
 [STAThreadAttribute]
