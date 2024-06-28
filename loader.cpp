03ed693766f8c325f6249c8dc2115b94a43cdeda
Kevin Crepps <kevinx.crepps@intel.com>
2024-06-27 19:44:37 -0700

    Implemented error reporting.

--- loader.cpp
+++ loader.cpp
@@ -1,6 +1,6 @@
 #include "loader.h"
 
-void Loader::SetPath(PATH_TYPE type, std::string path)
+void Loader::SetPath(PATH_TYPE type, std::string path) noexcept
 {
 	switch (type)
 	{
@@ -8,20 +8,27 @@ void Loader::SetPath(PATH_TYPE type, std::string path)
 		binPath = path;
 		break;
 	case PATH_LIB:
-		libPaths.push_back(path);
+		try
+		{
+			libPaths.push_back(path);
+		}
+		catch (...)
+		{
+			SetError("Exeception thrown while attempting to push lib path.");
+		}
 		break;
 	case PATH_RUN:
 		runPath = path;
 	}
 }
-//unsigned int Loader::ValidatePath(PATH_TYPE type) const
-//{
-//	switch (type)
-//	{
-//	case PATH_BIN:
-//		// If exists, return SUCCESS; else, return PATH_BIN
-//		return (unsigned int)type;
-//	}
-//
-//	return FAILURE_CONTINUE;
-//}
+unsigned int Loader::ValidatePath(PATH_TYPE type) const noexcept
+{
+	switch (type)
+	{
+	case PATH_BIN:
+		// If exists, return SUCCESS; else, return PATH_BIN
+		return (unsigned int)type;
+	}
+
+	return FAILURE_CONTINUE;
+}
