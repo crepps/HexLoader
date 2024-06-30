e25932348fde6f2789ee15c586d827e67f7804fb
Kevin Crepps <kevinx.crepps@intel.com>
2024-06-29 23:24:43 -0700

    Refactored tests, created second fixture. Started compiler tests.

--- loader.h
+++ loader.h
@@ -6,13 +6,15 @@
 #define SUCCESS 0
 #define FAILURE_ABORT 1
 #define FAILURE_CONTINUE 2
+#define BUFFER_SIZE 128
 
 class Loader
 {
 private:
 	std::string binPath,
 				runPath,
-				error;
+				error,
+				newProcessCmd;
 	std::vector<std::string> libPaths;
 	bool compilerInstalled,
 		compilerCheck;
@@ -29,15 +31,20 @@ public:
 	};
 
 	Loader() noexcept
-		:compilerInstalled(false), compilerCheck(false) {};
-	void SetError(std::string arg) noexcept { error = arg; }
+		:compilerInstalled(false),
+		compilerCheck(false),
+		newProcessCmd("choco install g++") {};
+	void SetError(const std::string& arg) noexcept { error = arg; }
 	std::string GetError() const noexcept { return error; }
-	void SetPath(PATH_TYPE, std::string) noexcept;
+	void SetPath(PATH_TYPE, const std::string&) noexcept;
 	unsigned int ValidatePath(PATH_TYPE) const noexcept;
 	std::string GetBinPath() const noexcept { return binPath; };
 	std::vector<std::string> GetLibPaths() const noexcept { return libPaths; };
 	std::string GetRunPath() const noexcept { return runPath; };
 	bool CompilerInstalled() noexcept;
 	bool CompilerChecked() const noexcept { return compilerCheck;  }
+	unsigned int InstallCompiler(std::string*) const noexcept;
+	void SetProcessCmd(const std::string& arg) { newProcessCmd = arg; }
+
 	~Loader() noexcept {}
 };
